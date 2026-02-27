f4cfd10dfd840663d827b5c78b5db88ee87b8ed0
David Theuri <d.mureithi.t@gmail.com>
2026-02-27 15:41:49 +0300

    add logic for displaying welcome message in js.

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-const submitButton = document.getElementById("name-submit-btn");
-const nameInputBox = document.getElementById("username");
+const nameButton = document.getElementById("name-submit-btn");
+const userName = document.getElementById("user-name");
 const welcomeSpan = document.getElementById("user-display");
 const itemInput = document.getElementById("itemName");
 const  itemLink = document.getElementById('itemLink');
 const itemPrice = document.getElementById('itemPrice');
 const itemStandout = document.getElementById('itemStandout');
 const itemCategory = document.getElementById('itemCategory');
-const addItem = document.getElementById('save-btn');
+const addItemBtn = document.getElementById('save-btn');
+
+nameButton.addEventListener('click' , () => {
+    const enteredName = userName.value;
+    welcomeSpan.textContent = enteredName;
+})
